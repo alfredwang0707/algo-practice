ea26f915b757ac9bceddc38330942202169c21c8
alfredwang0707 <alfredwang0707@gmail.com>
2021-05-11 22:03:19 -0400

    js practice

---
+++ front-end-questions.js
@@ -0,0 +1,24 @@
+var foo = "Hello";
+(function() {
+  var bar = " World";
+  alert(foo + bar);
+})();
+alert(foo + bar);
+
+
+// [wdqwdwqdbkdbkqbjndmw,dqw]
+
+// {n:1 , j:2}
+
+var result = {}
+    if [i] == result.i 
+    i.value +1 
+    else
+    [i]
+    //save key if it's first time and set to 1
+    //if same key 
+    //add value to to it
+    
+    var foo = {n: 1};
+    var bar = foo;
+    foo.x = foo = {n: 2};
