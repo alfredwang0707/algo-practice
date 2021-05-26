71a0ac4fb328dcc26e30dcc68c004ea0a9c2adea
alfredwang0707 <alfredwang0707@gmail.com>
2021-05-26 17:56:12 -0400

    hash table on common items

--- react-practice/commonitem.js
+++ react-practice/commonitem.js
@@ -18,7 +18,35 @@ function containsCommonItem(arr1,arr2){
     return false
 }
 
-containsCommonItem()
+//  
 //0(a * b)
-//
+// array1 ==> obj{
+// a: true
+// b: true
+// c: true
+// }
+// array2[index] --- obj.properties
+ 
+function containsCommonItem2(arr1,arr2){
+    // loop thru 1st array and create object where
+    // properties === items in the array
+    let map = {}
+    for (ley i=0; i < arr1.length; i++){
+        // if map.a exist, return true
+        if(!map[arr1[i]]){
+            const item =  arr1[i]
+            map[item] = true 
+            //map[a] = true
+            //continue until map a,b,c, all true
+            // loop thru 2nd array and check if item in 2nd arr exist on created obj 
+        }
+    }
+    for (let j=0; j < arr2.length; j++){
+        if (map[arr2[j]]){
+            return true
+        }
+    }
+    return false
+}
 
+//O (a + b) Time Complexity
