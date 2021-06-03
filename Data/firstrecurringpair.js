135a543432a7baa64b4acb124dc5fc6058ef03bc
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-03 14:16:07 -0400

    nested loop solution

---
+++ Data/firstrecurringpair.js
@@ -0,0 +1,21 @@
+// Given an array = [2,5,1,2,3,5,1,2,4]
+// it should return 2 
+
+//given an array [2,1,1,2,3,5,1,2,4]
+//should return 1 
+
+//given array [2,3,4,5]
+
+//brute froce nested loop
+
+function firstRecurringCharacter(input){
+    for (let i=0;i<input.length;i++){
+        for (let j= i+ 1;j<input.length;j++){
+            if(input[i] === input[j]){
+                return input[i]
+            }
+        } 
+    }
+    return undefined
+}
+
